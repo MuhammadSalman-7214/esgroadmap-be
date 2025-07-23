aaeb160f7012b4e5e787833f0c2f46ea457c44fc
Mehmood Akmal <akmalbajwa2672@gmail.com>
2025-07-23 12:20:55 +0500

    set cookies

--- api/server.js
+++ api/server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 const allowedorigins = [
+    'http://members.esgroadmap.com',
     'https://esgroadmap-frontend.vercel.app',
     'https://esgroadmap-fe-seven.vercel.app',
     'http://localhost:5173',
